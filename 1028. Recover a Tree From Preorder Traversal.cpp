c104af9c324bd8eef3bfa018551dd8c3129ecead
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2022-12-18 23:59:21 +0530

    18-12-22 Practice
    Hard

---
+++ 1028. Recover a Tree From Preorder Traversal.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int index = 0;
+
+    //root left right
+    TreeNode* helper(int level, string s){
+        //read number of dashes
+        int count = 0;
+        int i = index;
+        while(i < s.size() && s[i] == '-'){
+            count++;
+            i++;
+        }
+        //if number of dashes == level
+        //read the number and update index and create a node  
+        //else return null
+
+        if(count < level) return NULL;
+        
+        int num = 0;
+        while(i < s.size() && s[i] != '-'){
+            num *= 10;
+            num += s[i] - '0';
+            i++;
+        }
+
+        if(num == 0) return NULL;
+
+        TreeNode *node = new TreeNode(num);
+
+        index = i;
+
+        node -> left = helper(level + 1, s);
+
+        node -> right = helper(level + 1, s);
+
+        return node;
+    }
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        return helper(0, traversal);
+    }
+};
