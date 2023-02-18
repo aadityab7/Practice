3bb313069ee88f16e4a36053d229110e68a6f99f
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2023-02-18 15:19:19 +0530

    18-02-23 Practice
    Easy

--- 226. Invert Binary Tree.cpp
+++ 226. Invert Binary Tree.cpp
@@ -12,64 +12,12 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(!root) return root;
-        
-        queue<TreeNode *> q;
-        q.push(root);
-        TreeNode *node, *temp;
-        
-        while(!q.empty()){
-            node = q.front();
-            q.pop();
-                
-            temp = node -> left;
-            node -> left = node -> right;
-            node -> right = temp;
-            
-            if(node -> left){
-                q.push(node -> left);
-            }
-            
-            if(node -> right){
-                q.push(node -> right);
-            }
+        if(root){
+            swap(root -> left, root -> right);
+            invertTree(root -> left);
+            invertTree(root -> right);
         }
         
-        return root;
-    }
-};
-
-//USE IN - BUILT swap() function
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(!root) return root;
-        queue<TreeNode *> q;
-        q.push(root);
-        TreeNode *node, *temp;
-        while(!q.empty()){
-            node = q.front();
-            q.pop();
-            swap(node -> left, node -> right);
-            if(node -> left){
-                q.push(node -> left);
-            }
-            if(node -> right){
-                q.push(node -> right);
-            }
-        }
-        return root;
-    }
-};
-
-//USING RECURSION
-class Solution {
-public:
-    TreeNode* invertTree(TreeNode* root) {
-        if(!root) return root;
-        swap(root -> left, root -> right);
-        invertTree(root -> left);
-        invertTree(root -> right);
         return root;
     }
 };
