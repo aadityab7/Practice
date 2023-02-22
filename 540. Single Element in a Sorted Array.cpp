422948308ea7c83538b2ddee755b93461ec9c4d0
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2023-02-22 10:45:28 +0530

    21-02-23 Practice

---
+++ 540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        //start from 1st and 
+
+        for(int i = 0; i < nums.size() - 1; i += 2){
+            if(nums[i] != nums[i + 1]) return nums[i];
+        }
+
+        return nums[nums.size() - 1];
+    }
+};
