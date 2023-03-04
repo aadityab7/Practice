79ecf77d6c6bcf72523cee5555436425d09aeaa9
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2023-03-04 11:25:14 +0530

    03-03-23 Practice
    Medium

--- 28. Find the Index of the First Occurrence in a String.cpp
+++ 28. Find the Index of the First Occurrence in a String.cpp
@@ -1,4 +1,51 @@
 class Solution {
+    vector<int> create_lps(string pattern){
+        int n = pattern.size();
+        vector<int> lps(n, 0);
+        int i = 1, j = 0;
+
+        while(i < n){
+            if(pattern[i] == pattern[j]){
+                lps[i] = j + 1;
+                i++;
+                j++;
+            }
+            else{
+                if(j != 0) j = lps[j - 1];
+                else i++;
+            }
+        } 
+
+        return lps;
+    }
+
+public:
+    int strStr(string haystack, string needle) {
+        if(needle == "") return 0;
+
+        int n = haystack.size(), m = needle.size();
+        vector<int> lps = create_lps(needle);
+        int i = 0, j = 0;
+        while(i < n){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+                if(j == m) return i - m;
+
+            }
+            else{
+                if(j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+
+        return -1;
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
 public:
     int strStr(string haystack, string needle) {
         
