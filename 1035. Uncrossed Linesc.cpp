321e5dbec255cc9056af739d74f48dfa228c34b9
Aaditya Bansal <66950964+aadityab7@users.noreply.github.com>
2023-05-11 17:07:17 +0530

    11-05-23 Practice
    Medium

---
+++ 1035. Uncrossed Linesc.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        //LCS 
+        int n = nums1.size(), m = nums2.size();
+        int dp[n + 1][m + 1];
+
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= m; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(nums1[i - 1] == nums2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else{
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
